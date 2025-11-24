d1c0eabbd6f5b569efb443d3d34b5f42923635c5
melihkn <melih.sahinbas@sabanciuniv.edu>
2025-11-24 11:19:26 +0300

    some transforms are added to transforms.py and applied in dataset class and get_dataloader function in dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 import os
 from PIL import Image
 import numpy as np
+import data.transforms as T
 
 class ChangeDetectionDataset(Dataset):
     """
@@ -45,19 +46,42 @@ class ChangeDetectionDataset(Dataset):
         label = Image.open(path_label).convert('L') # Grayscale mask
         
         if self.transform:
-            # Apply transforms
-            # Note: For change detection, geometric transforms must be identical for A, B, and label
-            pass
+            img_A, img_B, label = self.transform(img_A, img_B, label)
         else:
-            # Default to tensor conversion
-            pass
-        import torchvision.transforms.functional as TF
-        img_A = TF.to_tensor(img_A)
-        img_B = TF.to_tensor(img_B)
-        label = TF.to_tensor(label)
+            img_A = T.ToTensor()(img_A)
+            img_B = T.ToTensor()(img_B)
+            label = T.ToTensor()(label)
             
         return {'image_A': img_A, 'image_B': img_B, 'label': label, 'filename': filename}
 
-def get_dataloader(root_dir, batch_size=8, split='train', num_workers=4):
-    dataset = ChangeDetectionDataset(root_dir, split=split)
+def get_dataloader(root_dir, batch_size=8, split='train', num_workers=4, transform=None):
+    """
+        Get dataloader for the dataset
+        Takes care of the data loading and preprocessing
+        If you want to add your own transform, pass it as an argument. Otherwise, below will be used.
+        Make sure that transforms.py file is used for your transformations since dataset class depends on its architecture.
+
+        Args:
+            root_dir (str): Root directory of the dataset
+            batch_size (int): Batch size
+            split (str): Split of the dataset (train/val)
+            num_workers (int): Number of workers for dataloader
+            transform (callable, optional): Optional transform to be applied on a sample.
+
+        Returns:
+            DataLoader: DataLoader for the dataset
+    """
+
+    if transform is None:
+        if split == 'train':
+            transform = T.Compose([
+                T.RandomHorizontalFlip(p=0.5),
+                T.RandomVerticalFlip(p=0.5),
+                T.RandomRotation(p=0.5),
+                T.ToTensor()
+            ])
+        else:
+            transform = T.ToTensor()
+        
+    dataset = ChangeDetectionDataset(root_dir, split=split, transform=transform)
     return DataLoader(dataset, batch_size=batch_size, shuffle=(split=='train'), num_workers=num_workers)
